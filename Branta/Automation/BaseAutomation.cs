ee5bf7670b2d4fac3771fcc11347a5ad92045df5
Kyle McCullen <kmccullen@protonmail.com>
2024-01-18 07:49:21 -0500

    use data binding

---
+++ Branta/Automation/BaseAutomation.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Threading;
+
+namespace Branta.Automation;
+
+public abstract class BaseAutomation : DispatcherObject
+{
+    public abstract int RunInterval { get; }
+
+    public abstract void Run();
+}
--- Branta/Enums/Color.cs
+++ Branta/Enums/Color.cs
@@ -5,12 +5,9 @@ namespace Branta.Enums;
 
 public sealed class Color : SmartEnum<Color>
 {
-    public static readonly Color Background = new(0, "#444444");
-    public static readonly Color BackgroundOffset = new(1, "#4c4c4c");
-    public static readonly Color White = new(2, "#ffffff");
-    public static readonly Color Gold = new (3, "#B1914A");
-    public static readonly Color Red = new (4, "#944545");
-    public static readonly Color Blue = new(5, "#547AA6");
+    public static readonly Color Gold = new (0, "#B1914A");
+    public static readonly Color Red = new (1, "#944545");
+    public static readonly Color Blue = new(2, "#547AA6");
 
     public Color(int value, string name) : base(name, value)
     {
