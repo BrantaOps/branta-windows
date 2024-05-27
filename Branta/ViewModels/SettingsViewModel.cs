eacd1c426fc93ce6acc027dd7e9d86764c083d6e
Kyle McCullen <kmccullen@protonmail.com>
2024-05-27 07:15:54 -0400

    cleanup

--- Branta/ViewModels/SettingsViewModel.cs
+++ Branta/ViewModels/SettingsViewModel.cs
@@ -149,10 +149,6 @@ public class SettingsViewModel : BaseViewModel
         WalletStatusChangeEnabled = settings.WalletVerification.WalletStatusChangeEnabled;
     }
 
-    public void OnRefreshRun()
-    {
-    }
-
     public Settings GetSettings()
     {
         return new Settings
