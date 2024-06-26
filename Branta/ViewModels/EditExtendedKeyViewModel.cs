287312cbe2b820cc7f16061126a05ff3e9b047e4
Kyle McCullen <kmccullen@protonmail.com>
2024-06-26 07:52:22 -0400

    extended key validation

--- Branta/ViewModels/EditExtendedKeyViewModel.cs
+++ Branta/ViewModels/EditExtendedKeyViewModel.cs
@@ -1,47 +1,102 @@
-﻿using Branta.Models;
+﻿using Branta.Commands;
+using Branta.Models;
 using Branta.Stores;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel.DataAnnotations;
 
 namespace Branta.ViewModels;
 
-public partial class EditExtendedKeyViewModel : ObservableObject
+public partial class EditExtendedKeyViewModel : ObservableValidator
 {
     private readonly ExtendedKeyStore _extendedKeyStore;
-    private readonly ExtendedKey _extendedKey;
+
+    public readonly ExtendedKey ExtendedKey;
 
     [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+    [Required(ErrorMessage = "Name is required.")]
+    [CustomValidation(typeof(EditExtendedKeyViewModel), nameof(ValidateIsNameUnique))]
     private string _name;
 
     [ObservableProperty]
+    [NotifyDataErrorInfo]
+    [NotifyCanExecuteChangedFor(nameof(SubmitCommand))]
+    [Required(ErrorMessage = "Extended Key is required.")]
+    [CustomValidation(typeof(EditExtendedKeyViewModel), nameof(ValidateIsValueUnique))]
+    [CustomValidation(typeof(EditExtendedKeyViewModel), nameof(ValidateExtendedKey))]
     private string _value;
 
 	public Action CloseAction { get; set; }
 
-    [RelayCommand]
+    public IEnumerable<ExtendedKey> ExtendedKeys => _extendedKeyStore.ExtendedKeys;
+
+    [RelayCommand(CanExecute = nameof(CanSubmit))]
     public void Submit()
     {
-        if (_extendedKey == null)
+        if (ExtendedKey == null)
         {
             _extendedKeyStore.Add(Name, Value);
         }
         else
         {
-            _extendedKeyStore.Update(_extendedKey.Id, Name, Value);
+            _extendedKeyStore.Update(ExtendedKey.Id, Name, Value);
         }
 
         CloseAction?.Invoke();
     }
 
+    public bool CanSubmit()
+    {
+        return Name != null && Value != null && !HasErrors;
+    }
+
     public EditExtendedKeyViewModel(ExtendedKeyStore extendedKeyStore, ExtendedKey extendedKey)
     {
         _extendedKeyStore = extendedKeyStore;
-        _extendedKey = extendedKey;
+        ExtendedKey = extendedKey;
 
-        if (_extendedKey != null)
+        if (ExtendedKey != null)
         {
-            Name = _extendedKey.Name;
-            Value = _extendedKey.Value;
+            Name = ExtendedKey.Name;
+            Value = ExtendedKey.Value;
         }
+    }
+
+    public static ValidationResult ValidateIsNameUnique(string value, ValidationContext context)
+    {
+        var viewModel = (EditExtendedKeyViewModel)context.ObjectInstance;
+
+        if (viewModel.ExtendedKey == null && viewModel.ExtendedKeys.Any(k => k.Name == value.Trim()))
+        {
+            return new("Name must be unique.");
+        }
+        
+        return ValidationResult.Success;
+    }
+
+    public static ValidationResult ValidateIsValueUnique(string value, ValidationContext context)
+    {
+        var viewModel = (EditExtendedKeyViewModel)context.ObjectInstance;
+
+        if (viewModel.ExtendedKey == null && viewModel.ExtendedKeys.Any(k => k.Value == value.Trim()))
+        {
+            return new("Extended Key must be unique.");
+        }
+        
+        return ValidationResult.Success;
+    }
+
+    public static ValidationResult ValidateExtendedKey(string value, ValidationContext context)
+    {
+        var viewModel = (EditExtendedKeyViewModel)context.ObjectInstance;
+
+        if (!ClipboardGuardianCommand.CheckForXPub(value.Trim()))
+        {
+            return new("xpub format is invalid.");
+        }
+        
+        return ValidationResult.Success;
     }
 }
